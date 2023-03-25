2b3d05848009f9488059f7a76767157fbaf0dd1a
chickenlittleish <snimer.25@acsamman.edu.jo>
2023-03-25 23:47:57 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,16 +1,46 @@
 import sys
 import time
 
-class Player():
-    #This is where the data related to the player like their name, their status, and etc
-    def __init__(self):
-            self.name = name
-            self.alive = True
-            self.age = age
+def Prologue():
+    print("'You must live no matter what, ∆µœåøŒ◊, as the successor of the--' A strange woman said as she bled out")
+    time.sleep(2)
+    print("'Keep up young master, we've almost escaped from them, soon, we should be able to leave the territory.' The old man you were walking with said.")
+    time.sleep(3)
+    print("'Young master, *cough* *cough*, I don't have much time left, listen carefully, take this, it's the technique the lord entrusted to me to give to you, you must protect it with your life.' He said as he handed you a book and pulled out his sword.")
+    time.sleep(5)
+    print("'Go! You must escape no matter what! So Go! Go!' He yelled as he stayed back to fight against the pursuers and you ran past him")
+    time.sleep(6)
+    print("'Finally I caught you, you bastard. You thought you could escape me, now that she's gone, all that's left is to get rid of you now and I'll be the victor.' A strange young man said as he laughed loudly")
+    time.sleep(7)
+    print("'Han Jue, tell that bastard Song Xū that we shall keep our part of the deal and send this bastard to him.' He said to one of the many men at the side.")
+    time.sleep(8)
+    print("[You wake up suddenly in a cold sweat feeling disoriented by the confusing dream.]")
+    time.sleep(3)
+    print("[You look around your surroundings confused as you realize that you're in some strange prison cell.]")
+    time.sleep(3)
+    print("[The walls are made of rock as if someone carved a room into a mountain. There's an old and worn out bed in the corner.]")
+    time.sleep(4)
+    print("[As you try to recall the confusing dream you had, you realize a disturbing fact. You have no clue who you are]")
+    time.sleep(4)
+    print("[You try your hardest to remember who you are? where are you from? But each time you try to remember your past, you get hit with a painful headache.]")
+    time.sleep(4)
+    print("[You decide to stop trying to remember your past for now and instead try to find that strange book you were given in your dream.]")
+    time.sleep(5)
+    print("(That wasn't just a dream, that felt too real to be a dream. That book must be here somewhere, it's the only link to my past.)")
+    time.sleep(8)
+    print("Where are you going to search for the book?")
+    print("Choices:\n 1)bed\n 2)yourself\n")
+    search_choice1 = input()
+    if search_choice1.lower() == "bed":
+        Scenario2()
+    if search_choice1.lower() == "yourself":
+        
+
+
+
+
+
+
 
 if __name__ == "__main__":
-    #ADD CAPTION
-    
-
-
-    #aggressiveness reaches ten, beats you up till your almost done security comes and he walks out and tells them to take you away
+    Prolouge()
